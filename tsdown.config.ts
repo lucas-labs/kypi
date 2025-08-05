7aa3513c8ad70503df68f469e73274fbaa7a84c5
Lucas Colombo <lucasncolombo@gmail.com>
2025-08-05 12:13:18 -0300

    chore: 🧹 add console logs for build process

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,11 @@
 import { codecovRollupPlugin } from '@codecov/rollup-plugin'
 import { defineConfig } from 'tsdown'
 
+console.info('\nBuilding kypi with tsdown...')
+console.info(
+  `📊 Bundle analysis is ${process.env.CODECOV_TOKEN ? 'ENABLED' : 'DISABLED'} for this build.\n`,
+)
+
 export default defineConfig({
   entry: ['src/index.ts', 'src/react/index.ts'],
   outDir: 'dist',
